5edbafce9bade7584f1e8ba3fa57b0fd8a0b2fb0
AndreCrescenzo <56078388+AndreCrescenzo@users.noreply.github.com>
2021-12-11 20:38:51 -0300

    Update main.py

--- cnp-pade/main.py
+++ cnp-pade/main.py
@@ -11,16 +11,16 @@ if __name__ == "__main__":
     agents = list()
 
     port = int(argv[1])
-    k = 1000
+    k = 1
     participants = list()
 
-    for m in range(2):
+    for m in range(50):
         agent_name = 'agent_participant_{port}@localhost:{port}'.format(port=port+(m+1)*k)
         ag_participant = AgentParticipant(AID(name=agent_name))
         participants.append(agent_name)
         agents.append(ag_participant)
 
-    for n in range(2):
+    for n in range(200):
         agent_name = 'agent_contractor_{port}@localhost:{port}'.format(port=port-n*k)
         ag_contractor = AgentContractor(AID(name=agent_name), participants, i=1)
         agents.append(ag_contractor)
