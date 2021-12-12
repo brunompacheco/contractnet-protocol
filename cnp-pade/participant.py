d4f6e03d4445f5b28a0f7520262de237cf0752dd
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2021-12-11 21:19:42 -0300

    drop task execution time; price varies with time

--- cnp-pade/participant.py
+++ cnp-pade/participant.py
@@ -10,30 +10,18 @@ class BehaviourParticipant(FipaContractNetProtocol):
     def __init__(self, agent, message=None):
         super().__init__(agent, message=message, is_initiator=False)
 
-        self.available = True
-
     def display(self, msg: str):
         return display_message(self.agent.aid.name, msg)
 
     def handle_cfp(self, message):
         super().handle_cfp(message)
 
-        disp = f"Call for proposals received for contract '{message.content}', "
-        if self.available:
-            disp += "I'm available!"
+        # self.display(f"Call for proposals received for contract '{message.content}'")
 
-            answer = message.create_reply()
-            answer.set_performative(ACLMessage.PROPOSE)
-            answer.set_content(self.agent.price)
-            self.agent.send(answer)
-        else:
-            disp += "but I'm not available"
-
-            answer = message.create_reply()
-            answer.set_performative(ACLMessage.REFUSE)
-            self.agent.send(answer)
-
-        self.display(disp)
+        answer = message.create_reply()
+        answer.set_performative(ACLMessage.PROPOSE)
+        answer.set_content(self.agent.price())
+        self.agent.send(answer)
 
     def handle_reject_propose(self, message):
         super().handle_reject_propose(message)
@@ -47,12 +35,7 @@ class BehaviourParticipant(FipaContractNetProtocol):
 
         self.do_task(message)
 
-    def do_task(self, message):
-        if self.available:
-            self.available = False
-            call_later(self.agent.task_length, self.inform_done, message)
-        else:
-            call_later(0, self.do_task, message)
+        self.inform_done(message)
 
     def inform_done(self, message):
         self.display(f"Finished contract '{message.content}'")
@@ -62,20 +45,13 @@ class BehaviourParticipant(FipaContractNetProtocol):
         answer.set_content("DONE")
         self.agent.send(answer)
 
-        self.available = True
-
 class AgentParticipant(Agent):
     def __init__(self, aid, price=None, task_length=None, debug=False):
         super().__init__(aid, debug=debug)
 
         if price is None:
-            self.price = 10 * random() + 100
+            self.price = lambda: 10 * random() + 100
         else:
             self.price = price
-        
-        if task_length is None:
-            self.task_length = 0 #0.5 + random() * 0.5
-        else:
-            self.task_length = task_length
 
         self.behaviours.append(BehaviourParticipant(self))
